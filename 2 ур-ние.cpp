982804d551d072a15f32788114f89a5fadd15c27
barvenov <barvenovsa@gmail.com>
2015-10-18 21:32:01 +0300

    Недочеты в программе

--- 2 ур-ние.cpp
+++ 2 ур-ние.cpp
@@ -6,7 +6,7 @@
 using std::cout;
 using std::cin;
 using std::endl;
-using std::sqrt;
+using std::sqrt; // это про что??????
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -18,13 +18,14 @@ int main()
 	cout << " введите коэфициент ñ= ";
 	cin >> c;
 	D = b*b - 4 * a * c;
+	// а если уравнение НЕ квадратное?
 	if (D > 0){
 		x1 = -b + sqrt(D) / (2 * a);
 		x2 = -b - sqrt(D) / (2 * a);
 		cout << "x1= " << x1 << endl << "x2= " << x2 << endl;
 	}
 	else {
-		if (D == 0){
+		if (D == 0){ // не очень хорошее сравнение!!!
 			x1 = -b / (2 * a);
 			cout << "x1= " << x1 << endl;
 		}
