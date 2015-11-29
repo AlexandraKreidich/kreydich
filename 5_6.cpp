71838f7a151714df9e97f1f883fd8178b2450d0b
Kreidich Alexandra <Kreidichalexandra@gmail.com>
2015-11-30 02:44:09 +0300

    5_6

---
+++ 5_6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <clocale>
+using namespace std;
+void initArray(char A[], char B[], const int N){
+	for (int i = 0; i < N; i++){
+		A[i] = B[rand() % 4];
+	}
+}
+void showArray(char A[], const int N){
+	for (int i = 0; i < N; i++){
+		cout << A[i] << endl;
+	}
+}
+int main(){
+	setlocale(LC_ALL, "russian");
+	const int N = 7;
+	char A[N];
+	char B[] = { 'Т', 'К', 'Д', 'В' };
+	initArray(A, B, N);
+	showArray(A, N);
+	return 0;
+}
