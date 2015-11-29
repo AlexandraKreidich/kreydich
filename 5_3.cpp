022305efbb9f5d9ee0fec73fa6314186b658ec82
Kreidich Alexandra <Kreidichalexandra@gmail.com>
2015-11-30 00:59:45 +0300

    new

--- 5_3.cpp
+++ 5_3.cpp
@@ -15,14 +15,24 @@ void show_array(int A[], const int N){
 		cout << A[i] << endl;
 	}
 }
-//сдвик на к элементов
-void array(int A[], const int N, int k){
+//сдвиг на к элементов вправо
+void arrayRight(int A[], const int N, int k){
 	for (int i = 0; i < k; i++){
 		int b = A[N - 1];
 		for (int j = N - 1; j > 0; j--){
 			A[j] = A[j - 1];
 		}
 		A[0] = b;
+	}
+}
+//сдвиг на к элементов влево
+void arrayLeft(int A[], const int N, int k){
+	for (int i = 0; i < k; i++){
+		int b = A[0];
+		for (int j = 0; j < N; j++){
+			A[j] = A[j + 1];
+		}
+		A[N - 1] = b;
 	}
 }
 int main(){
@@ -33,11 +43,16 @@ int main(){
 	system("cls");
 	cout << "array" << endl;
 	show_array(A, N);
-	int k = 0;
-	cout << "input k: " << endl;
-	cin >> k;
-	array(A, N, k);
-	cout << "new array: " << endl;
+	int k1 = 0, k2 = 0;
+	cout << "input k1: " << endl;
+	cin >> k1;
+	arrayRight(A, N, k1);
+	cout << "new array right: " << endl;
+	show_array(A, N);
+	cout << "input k2: " << endl;
+	cin >> k2;
+	arrayLeft(A, N, k2);
+	cout << "new array left: " << endl;
 	show_array(A, N);
 	return 0;
 }
--- 5_4.cpp
+++ 5_4.cpp
@@ -28,7 +28,7 @@ int sumDiag(int *A, const int N, const int M){
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < M; j++){
 			if (i == j){
-				sum1 += *(A+i*N+j);
+				sum1 += *(A + i*N + j);
 			}
 		}
 	}
@@ -45,12 +45,38 @@ int sumUnderDiag(int *A, const int N, const int M){
 	}
 	return sum2;
 }
+void sedlovayaPoint(int*A, const int N, const int M){
+	int jsedlo = 0;
+	bool check = true;
+	for (int i = 0; i < N; i++){
+		jsedlo = 0;
+		for (int j = 0; j < M; j++){
+			if (*(A + i*N + j) < *(A + i*N + jsedlo)){
+				jsedlo = j;
+			}
+		}
+		check = true;
+		for (int i2 = 0; i2 < N; i2++){
+			if (*(A + i2*N + jsedlo) > *(A + i*N + jsedlo)){
+				check = false;
+				break;
+			}
+		}
+		if (check){
+			cout << "[" << i << ", " << jsedlo << "] = " << *(A + i*N + jsedlo) << endl;
+			break;
+		}
+	}
+	if (!check){
+		cout << "Седловых точек нет!" << endl;
+	}
+}
 int main(){
 	setlocale(LC_ALL, "russian");
-	const int N = 5, M = 5;
-	int A[N][M];
+	const int N = 7, M = 7;
+	int A[N][M]; //= { {3,4,5}, {2,1,3}, {1,4,2} };
 	initArray(*A, N, M);
 	showArray(*A, N, M);
-	cout << "сумма диагонали: " << sumDiag(*A,N,M) << endl;
-	cout << "сумма элементов нижнего треугольника: " << sumUnderDiag(*A,N,M) << endl;
-}
+	cout << "сумма диагонали: " << sumDiag(*A, N, M) << endl;
+	cout << "сумма элементов нижнего треугольника: " << sumUnderDiag(*A, N, M) << endl;
+	sedlovayaPoint(*A, N, M);
