8edfdc8884a2ff2c548c50092f12928b7ef81235
Kreidich Alexandra <Kreidichalexandra@gmail.com>
2015-11-30 03:04:26 +0300

    5_7

--- 5_7.cpp
+++ 5_7.cpp
@@ -21,15 +21,13 @@ int main(){
 	int A[N] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	showArray(A, N);
 	int k1 = 0, k2 = 0;
-	k1 = 1 + rand() % 10;
-	k2 = 1 + rand() % 10;
-	cout << "k1= " << k1 << endl;
-	cout << "k2= " << k2 << endl;
 	int n = 0;
 	cout << "input n " << endl;
 	cin >> n;
 	system("cls");
 	for (int i = 0; i < n; i++){
+		k1 = 1 + rand() % 10;
+		k2 = 1 + rand() % 10;
 		swap(A[k1],A[k2]);
 	}
 	showArray(A, N);
