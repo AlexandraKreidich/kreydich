fc95e415201da7c38e97310933b5509213c5aea8
akreidich <kreidichalexandra@gmail.com>
2016-05-27 12:30:59 +0300

    class_Point2D_added

---
+++ class_Point2D.cpp
@@ -0,0 +1,130 @@
+﻿//точка на плоскости
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	Point();
+	Point(double, double);
+	Point(double);
+	Point(const Point&);
+	Point operator+(const Point&);
+	Point operator+(int);
+	Point operator*(int);
+	Point operator=(Point&);
+	double sqr(double);
+	double dist(Point&, Point&);
+	friend ostream& operator<<(ostream&, const Point&);
+	friend istream& operator>>(istream&, Point&);
+	~Point();
+};
+
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+	Point D;
+	Point C = D;
+	cout << "Point C = D:" << " C" << C << endl;
+	Point E(3, 5);
+	cout << "Point E = " << "E" << E << endl;
+	Point S = E * 2;
+	cout << "Point S = E * 2:" << " S" << S << endl;
+	Point U = S + E;
+	cout << "Point U = S + D:" << " U" << U << endl;
+	Point T = E + 3;
+	cout << "Point T = E + 3:" << " T" << T << endl;
+	Point I;
+	I = I;
+	cout << "Point I = I:" << " I" << I << endl;
+	Point f[4] = { {1,3}, {2,3} , {4,7}, {3,5} };
+	for (int i = 0; i < 4; i++) {
+		cout << "f[" << i+1 << "] = " << f[i] << endl;
+	}
+	Point G;
+	cout << "введите точку G: " << endl;
+	cin >> G;
+	cout << "Point G" << G << endl;
+}
+
+Point::Point() : x(0), y(0) {}
+
+Point::Point(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+Point::Point(const double x)
+{
+	this->x = x;
+	this->y = x;
+}
+
+Point::Point(const Point& D)
+{
+	x = D.x;
+	y = D.y;
+}
+
+Point Point::operator+(const Point & D)
+{
+	Point tmp;
+	tmp.x = this->x + D.x;
+	tmp.y = this->y + D.y;
+	return tmp;
+}
+
+Point Point::operator+(int i)
+{
+	this->x += i;
+	this->y += i;
+	return *this;
+}
+
+Point Point::operator*(int i)
+{
+	this->x *= i;
+	this->y *= i;
+	return *this;
+}
+
+Point Point::operator=(Point & D)
+{
+	this->x = D.x;
+	this->y = D.y;
+	return *this;
+}
+double Point::sqr(double x)
+{
+	return x*x;
+}
+double Point::dist(Point & D, Point & C)
+{
+	double Dist = 0.0;
+	Dist = sqrt(sqr(C.x - D.x) + sqr(C.y - D.y));
+	return Dist;
+}
+
+ostream & operator<<(ostream &out, const Point &A)
+{
+	out << "(" << A.x << "," << A.y << ")" << endl;
+	return out;
+}
+
+istream & operator >> (istream &in, Point &A)
+{
+	cout << "введите координату x" << endl;
+	in >> A.x;
+	cout << "введите координату y" << endl;
+	in >> A.y;
+	return in;
+}
+Point::~Point() {}
